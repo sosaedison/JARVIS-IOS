737d1cf64f83d50fa0c2df5cbc0c47d95ac1c033
Sosa Edison <17048396+sosaedison@users.noreply.github.com>
2024-04-20 10:02:02 -0500

    Initial Commit

---
+++ JARVIS-IOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JARVIS-IOS
+//
+//  Created by Sosa Edison on 4/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JARVIS-IOS/JARVIS_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  JARVIS_IOSApp.swift
+//  JARVIS-IOS
+//
+//  Created by Sosa Edison on 4/20/24.
+//
+
+import SwiftUI
+
+@main
+struct JARVIS_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
